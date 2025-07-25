5188919c5a69b7f4e87a502b15de82460d7b73d7
Swastika Tikader <143022383+swastika49@users.noreply.github.com>
2025-07-25 23:03:21 +0530

    Create App.js

---
+++ week 6/3. ReactJS-HOL/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore />
+    </div>
+  );
+}
+
+export default App;
