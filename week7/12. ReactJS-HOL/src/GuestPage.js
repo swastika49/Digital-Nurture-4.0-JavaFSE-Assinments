bfa7f9cc61e8c46051b0bb91d6e88a9e9471a3d3
Swastika Tikader <143022383+swastika49@users.noreply.github.com>
2025-08-02 17:57:20 +0530

    Create GuestPage.js

---
+++ week7/12. ReactJS-HOL/src/GuestPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GuestPage = () => {
+  return (
+    <div>
+      <h2>Welcome, Guest!</h2>
+      <p>Browse flights and deals. Please login to book your tickets.</p>
+    </div>
+  );
+};
+
+export default GuestPage;
